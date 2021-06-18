17857f5c498e4fd63267739150f38944cf44a263
Tyree <t9tyree@hotmail.co.uk>
2021-06-18 10:12:15 +0100

    extra file added

---
+++ Customer_order.py
@@ -0,0 +1,8 @@
+class Customer_order:
+
+    Order = [
+
+    ]
+
+    def AddToMenu(self, Order):
+        return Customer_order.Order.append(Order)
--- Resturant_Customer.py
+++ Resturant_Customer.py
@@ -1,5 +1,18 @@
 from Resturant_Menu import Menu
+from Customer_order import Customer_order
 
 
-class Customer(Menu):
-    print(Menu().Resturant_menu[int(input("What do you want from the menu?    "))])
+class Customer(Menu, Customer_order):
+
+    def Customer_Order(self):
+        print("On todays menu we have the following:")
+        for items in Menu().Resturant_menu.values():
+            print(items)
+        new_item = input("What do you want from the menu?    ")
+        Customer_order().AddToMenu(Menu().Resturant_menu[ int(new_item) ])
+        # print(Menu().Resturant_menu[int(input("What do you want from the menu?    "))])
+        print(Customer_order().Order)
+
+
+while True:
+    Customer().Customer_Order()
